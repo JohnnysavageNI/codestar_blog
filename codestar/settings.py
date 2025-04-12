9592fd774a150b4358540e6477867718a10a8480
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-04-12 17:57:10 +0100

    Add allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x3=%zxz0r6d4la!0c6@o+w@=a+jo_xx=xvb3f@1ya7k28+pvr5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 'django-blog-jms-e9f36faa8b35.herokuapp.com',
+                 '127.0.0.1',]
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
