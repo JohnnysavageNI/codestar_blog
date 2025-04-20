0058a27caf3888f45b6a391b412960e321e13588
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-04-20 17:02:16 +0100

    deployment settings, first user

---
+++ env.py
@@ -0,0 +1,11 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",
+    (
+        "postgresql://neondb_owner:"
+        "npg_dCPaS5KZ7bqm@"
+        "ep-hidden-shape-a296isjz.eu-central-1.aws.neon.tech/"
+        "navy_mash_panda_31686"
+    )
+)
