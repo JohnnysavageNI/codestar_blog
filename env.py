24e31e3d8eb5b2d3951b18a91fc661049aef2952
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-04-20 17:12:52 +0100

    Update database url

--- env.py
+++ env.py
@@ -1,11 +1,4 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL",
-    (
-        "postgresql://neondb_owner:"
-        "npg_dCPaS5KZ7bqm@"
-        "ep-hidden-shape-a296isjz.eu-central-1.aws.neon.tech/"
-        "navy_mash_panda_31686"
-    )
-)
+    "DATABASE_URL", "<postgresql://neondb_owner:npg_dCPaS5KZ7bqm@ep-hidden-shape-a296isjz.eu-central-1.aws.neon.tech/navy_mash_panda_31686")
