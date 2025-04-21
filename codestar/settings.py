4af15a830342271f65c2fc92cb70569ee0743dfc
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-04-21 19:49:35 +0100

    fix error in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("4#eg*1h0-#*=q=1^!r5_7(3v)5&mwq32ra-*se6)jqoqal)k")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "<postgresql://neondb_owner:npg_dCPaS5KZ7bqm@ep-hidden-shape-a296isjz.eu-central-1.aws.neon.tech/navy_mash_panda_31686")
+os.environ.setdefault("SECRET_KEY", "<4#eg*1h0-#*=q=1^!r5_7(3v)5&mwq32ra-*se6)jqoqal)k>")
